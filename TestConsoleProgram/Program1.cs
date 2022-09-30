0ac8d2131773153e67ffdd103c3de1519352e1dc
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-09-30 00:16:01 -0700

    Test2
    Simple calculator

---
+++ TestConsoleProgram/Program1.cs
@@ -0,0 +1,19 @@
+﻿// write simple calculator
+/*Console.WriteLine("math calkulator");
+int a, b, c ;
+Console.WriteLine("Write first number:");
+a = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Write second number:");
+b = Convert.ToInt32 (Console.ReadLine());
+
+c = a + b;
+
+Console.WriteLine("Our result is:" + c);*/
+
+string str = "66";
+int a;
+int b = Convert.ToInt32(str);
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(b + a);
+
